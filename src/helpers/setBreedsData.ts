fa3d4fc8420aebc24abff71cede67d450257fb55
Marco <marcoreynarinaldi@gmail.com>
2023-03-26 22:31:27 +0200

    changed image paths again for github pages

--- src/helpers/setBreedsData.ts
+++ src/helpers/setBreedsData.ts
@@ -1,25 +1,29 @@
 import { IApiItem, IItemData } from "../types/index";
+import rottweilerImg from "../assets/rottweiler.svg";
+import dachshundImg from "../assets/dachshund.svg";
+import bulldogImg from "../assets/bulldog.svg";
+import greyhoundImg from "../assets/greyhound.svg";
 
 const breeds: IItemData[] = [
   {
     heading: "Rottwe",
-    img: "src/assets/rottweiler.svg",
+    img: rottweilerImg,
     bodyText: "",
   },
   {
     heading: "Dachshund",
-    img: "src/assets/dachshund.svg",
+    img: dachshundImg,
     bodyText:
       "Woof Woof (Dachshund breed not found in the DogApi or any other related breed)",
   },
   {
     heading: "American Bulld",
-    img: "src/assets/bulldog.svg",
+    img: bulldogImg,
     bodyText: "",
   },
   {
     heading: "Greyh",
-    img: "src/assets/greyhound.svg",
+    img: greyhoundImg,
     bodyText: "",
   },
 ];
